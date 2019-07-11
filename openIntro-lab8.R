b8ce602097ccba2ba22a2db3ea92468ec57a9204
Madhu Vasudevan <madhuvasudevan@yahoo.com>
2019-07-11 16:41:00 -0500

    added more labs

--- openIntro-lab8.R
+++ openIntro-lab8.R
@@ -19,3 +19,24 @@ abline(m_bty)
 hist(m_bty$residuals)
 plot(m_bty$residuals ~ evals$bty_avg)
 abline(h = 0, lty = 3) # adds a horizontal dashed line at y = 0
+
+plot(evals$bty_avg ~ evals$bty_f1lower)
+cor(evals$bty_avg, evals$bty_f1lower)
+
+head(evals[, 13:19])
+plot(evals[, 13:19])
+
+m_bty_gen <- lm(score ~ bty_avg + gender, data = evals)
+summary(m_bty_gen)
+
+multiLines(m_bty_gen)
+
+m_full <- lm(score ~ rank + ethnicity + gender + language + age + cls_perc_eval 
+             + cls_students + cls_level + cls_profs + cls_credits + bty_avg 
+             + pic_outfit + pic_color, data = evals)
+summary(m_full)
+
+
+
+
+
